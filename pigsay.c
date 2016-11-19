f8ae7d30eed3229cccfb7648ddf16d550a1dd8df
Felix Zhang <zhangxufei05@hotmail.com>
2016-11-19 13:07:37 +0800

    first model

---
+++ pigsay.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+  printf("  /\\_____/\\    -----------------------------\n");
+  printf(" /         \\  |%-29s|\n", argv[1]);
+  printf(" | O     O |  /-----------------------------\n");
+  printf(" |   ___   | /\n");
+  printf(" \\  |o_o|  /\n");
+  printf("  \\_______/\n");
+
+  exit(0);
+}
