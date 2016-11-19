bc1caf14bfdcdf7f36b3c833a8231464f667da5c
Felix Zhang <zhangxufei05@hotmail.com>
2016-11-19 13:10:31 +0800

    no need to add "" now

--- pigsay.c
+++ pigsay.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
+  char *text;
+  int i, len=0;
+  for (i=1; i<argc; i++) {
+    len += strlen(argv[i]);
+    len +=1;
+  }
+  text = malloc(len);
+  for(int i=1; i<argc; i++){
+    strcat(text, argv[i]);
+    if (i != argc -1) {
+      strcat(text, " ");
+    }
+  }
+  len -= 1;
   printf("  /\\_____/\\    -----------------------------\n");
-  printf(" /         \\  |%-29s|\n", argv[1]);
+  printf(" /         \\  |%-29s|\n", text);
   printf(" | O     O |  /-----------------------------\n");
   printf(" |   ___   | /\n");
   printf(" \\  |o_o|  /\n");
